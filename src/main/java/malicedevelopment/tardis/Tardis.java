66d1e0d9302be8ccc55be801276f78da8bf62156
Bigjango13 <bigjango13@gmail.com>
2024-09-06 07:45:02 -0700

    Setup

--- src/main/java/malicedevelopment/tardis/Tardis.java
+++ src/main/java/malicedevelopment/tardis/Tardis.java
@@ -1,4 +1,4 @@
-package turniplabs.examplemod;
+package malicedevelopment.tardis;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -8,12 +8,12 @@ import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
-public class ExampleMod implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
-    public static final String MOD_ID = "examplemod";
+public class Tardis implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
+    public static final String MOD_ID = "tardis";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
     public void onInitialize() {
-        LOGGER.info("ExampleMod initialized.");
+        LOGGER.info("Tardis initialized.");
     }
 
 	@Override
