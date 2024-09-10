4d6eb714708ddea2d9cb409867dc2b8ea0090026
Bigjango13 <bigjango13@gmail.com>
2024-09-10 14:20:41 -0700

    Added model

--- src/main/java/malicedevelopment/tardis/block/tardisblock.java
+++ src/main/java/malicedevelopment/tardis/block/tardisblock.java
@@ -1,16 +1,23 @@
 package malicedevelopment.tardis.block;
 
+import static malicedevelopment.tardis.Tardis.MOD_ID;
 import malicedevelopment.tardis.Tardis;
 import net.minecraft.core.block.Block;
 import net.minecraft.core.block.material.Material;
+import org.useless.dragonfly.model.block.DFBlockModelBuilder;
 import turniplabs.halplibe.helper.BlockBuilder;
 
 public class tardisblock {
     public static Block TARDISfuncblock;
 
     public static void init() {
-        TARDISfuncblock = new BlockBuilder(Tardis.MOD_ID)
-			
+        TARDISfuncblock = new BlockBuilder(MOD_ID)
+            .setTextures("tardis:block/cinnabar_ore")
+			.setBlockModel(block -> new DFBlockModelBuilder(MOD_ID)
+                .setBlockModel("model.json")
+                //.setBlockState("network_cable.json")
+                .build(block)
+            )
 			.build(new Block("TARDIS", 3600, Material.metal));
 
 	}
